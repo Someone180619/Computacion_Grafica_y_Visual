c502005501a5caf6f17a7a62b6dc4970cd06f7e2
Oriel Delgado <orieldelgado01@gmail.com>
2022-05-11 13:22:50 -0500

    [LAB4] Se ha añadido iluminación ambiente, difusa, especular y se han realizado cierto grupo de comentarios.

--- LB4/main.cpp
+++ LB4/main.cpp
@@ -6,30 +6,14 @@ using namespace std;
 
 float a, b;
 
-void iniciarProyeccion ()
+void iniciarProyeccion (void)
 {
     glClearColor(1.0,1.0,1.0,0.0);
     glMatrixMode(GL_MODELVIEW | GL_PROJECTION);
     glLoadIdentity();
 }
 
-/*void GradientBackground(){
-
-    glBegin(GL_QUADS);
-    //celeste
-    glColor3f(0.5f, 1.0f, 1.0f);
-    glVertex2f(-1.0, 1.0);
-    glVertex2f(-1.0,-1.0);
-    //amarillo
-    glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex2f(1.0,-1.0);
-    glVertex2f(1.0, 1.0);
-    glEnd();
-
-
-}*/
-
-void propellers(void){
+void propellers(void){ //Hélices del avión tanto derecha, como izquierda.
     float calx, caly, r = 0.07, px_hp = -0.318, py_hp = 0.25, px_lp = 0.128, py_lp = -0.139;
     glColor3f(0.0, 0.0, 0.0);
     //High Propeller
@@ -46,6 +30,7 @@ void propellers(void){
     }
     glEnd();
     glPopMatrix();
+    glColor3f(0.0, 0.2, 0.0);
     glBegin(GL_POLYGON);
     glVertex2f(-0.333, 0.25);
     glVertex2f(-0.318, 0.265);
@@ -55,6 +40,7 @@ void propellers(void){
 
     //Lower Propeller
     glPushMatrix();
+    glColor3f(0.0, 0.0, 0.0);
     glTranslatef(0.128, -0.139, 0.0);
     glRotatef(b,0,0,1);
     glTranslatef(-0.128, 0.138, 0.0);
@@ -67,6 +53,7 @@ void propellers(void){
     }
     glEnd();
     glPopMatrix();
+    glColor3f(0.0, 0.2, 0.0);
     glBegin(GL_POLYGON);
     glVertex2f(0.113, -0.139);
     glVertex2f(0.128, -0.154);
@@ -75,11 +62,11 @@ void propellers(void){
     glEnd();
 }
 
-void internalChambers(void)
+void internalChambers(void) //Capa interna de las turbinas.
 {
     float x, y, r = 0.07, i = 0.0f;
     //High Chamber
-    glColor3f(0.0, 1.0, 0.0);
+    glColor3f(0.3, 0.3, 0.3);
     glBegin(GL_TRIANGLE_FAN);
     glVertex2f(x = -0.318, y = 0.25); // Center
     for(i = 0.0f; i <= 360; i++)
@@ -94,10 +81,14 @@ void internalChambers(void)
     propellers();
 }
 
-void externalChambers(void)
+void externalChambers(void) //Capa externa de las turbinas.
 {
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    glDisable(GL_LIGHTING);
+    GLfloat luz_ambiente_ch[]={0.1,0.1,0.1,1.0};
+    GLfloat luz_difusa_ch[]={0.8,0.8,0.8,1.0};
+    GLfloat luz_especular_ch[]={0.4,0.4,0.4,1.0};
+    GLfloat luz_alta[]={0.0,0.2,-0.8,1.0};
     glBegin(GL_QUADS);
     //celeste
     glColor3f(0.650, 0.878, 0.890);
@@ -111,7 +102,13 @@ void externalChambers(void)
 
     glColor3f(0.4, 0.4, 0.4);
     //High chamber
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
     glPushMatrix();
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luz_ambiente_ch);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luz_difusa_ch);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luz_especular_ch);
+    glLightfv(GL_LIGHT0, GL_POSITION, luz_alta);
     glTranslatef(-0.29, 0.26, 0.0);
     glutSolidSphere(0.1, 25, 20);
     glPopMatrix();
@@ -120,20 +117,22 @@ void externalChambers(void)
     glTranslatef(0.15, -0.12, 0.0);
     glutSolidSphere(0.1, 25, 20);
     glPopMatrix();
-
+    glDisable(GL_LIGHTING);
     internalChambers();
 
 
 }
 
-static void display(void)
+static void display(void) //Función encargada de la construcción en general de la estructura del avión.
 {
+    GLfloat luz_ambiente[]={0.30, 0.30, 0.35, 1.0};
+    GLfloat luz_difusa[]={0.30, 0.30, 0.50, 1.0};
+    GLfloat luz_especular[]={0.70, 0.70, 0.80, 1.0};
+    GLfloat luz_baja[]={0.0,0.2,-0.8,1.0};
     float wx = -0.25, wy = -0.1, cx = 0.0, cy = 0.0;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //Chambers
-    //GradientBackground();
     externalChambers();
-
     //Wing
     glBegin(GL_POLYGON);
     //high
@@ -181,6 +180,7 @@ static void display(void)
 
 
     //Main Cabin
+    glDisable(GL_LIGHTING);
     glPushMatrix();
     glColor3f(0.0, 0.2, 0.4);
     glTranslatef(-0.53, -0.205, 0.0);
@@ -202,14 +202,20 @@ static void display(void)
     glTranslatef(-0.45, -0.15, 0.0);
     glutSolidSphere(0.025, 25, 20);
     glPopMatrix();
+    glDisable(GL_LIGHTING);
     glBegin(GL_POLYGON);
     glVertex2f(-0.6, -0.175);
     glVertex2f(-0.45, -0.125);
     glVertex2f(-0.45, -0.175);
     glVertex2f(-0.6, -0.225);
     glEnd();
-
     glPushMatrix();
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luz_ambiente);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luz_difusa);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luz_especular);
+    glLightfv(GL_LIGHT0, GL_POSITION, luz_baja);
     glTranslatef(-0.35, -0.2, 0.0);
     glutSolidSphere(0.035, 25, 20);
     glPopMatrix();
@@ -239,12 +245,12 @@ static void display(void)
 void specialkey(int key, int x, int y)
 {
     if(key==GLUT_KEY_UP|GLUT_KEY_DOWN|GLUT_KEY_RIGHT|GLUT_KEY_LEFT)
-        a-=10;
-        b+=10;
+        a-=10; //Hélices altas
+        b+=10; //Hélices bajas
         glutPostRedisplay();
 }
 
-int main (int argc, char* argv [])
+int main (int argc, char* argv []) //Función principal.
 {
 
     glutInit(&argc,argv);
